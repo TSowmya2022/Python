ff730cc037feff92064c5a0c55cbe11993fe1057
Sowmya Talari <93229436+TSowmya2022@users.noreply.github.com>
2021-11-02 22:03:16 +0000

    Add files via upload

---
+++ Python 2 Task1.py
@@ -0,0 +1,6 @@
+num = int(input("Please enter your number "))
+#Taking input from user
+if num>10:
+    print("Number is Greater than 10")
+else:
+    print("Number is Smaller than 10")
---
+++ Python 2 Task2.py
@@ -0,0 +1,12 @@
+number = int(input("Please enter your number "))
+while number<10:
+    print(number)
+    number = number + 1
+else:
+    print('Loop ended')
+
+
+
+
+
+
